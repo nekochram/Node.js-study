6b6216ba3303be90ec738bff4b522b130b08a103
helei_Eri <9933843+helei-eri@user.noreply.gitee.com>
2023-08-03 16:27:49 +0800

    expree文件上传

---
+++ express-learn/express-generator/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const formidable = require('formidable');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/portrait', function(req, res, next) {
+    res.render('portrait');
+});
+
+router.post('/portrait', function(req, res, next) {
+    const form = formidable.formidable({
+        multiples:true,
+        uploadDir:__dirname + '/../public/images',
+        keepExtensions:true
+    });
+
+  form.parse(req, (err, fields, files) => {
+    let url=''
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(files)
+    url=`/images/${files.avatar[0].newFilename}`
+    res.end(url);
+  });
+});
+
+module.exports = router;
